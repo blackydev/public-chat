532b33eec7102334ed8165b542c0b0546135e4e1
Blackky <patryklikus@icloud.com>
2024-04-12 00:36:14 +0200

    create PostgresqlClient

---
+++ src/main/java/com/patryklikus/publicchat/controllers/UserController.java
@@ -0,0 +1,9 @@
+/* Copyright Patryk Likus All Rights Reserved. */
+package com.patryklikus.publicchat.controllers;
+
+import com.patryklikus.publicchat.https.request.methodMappings.RequestMapping;
+
+@RequestMapping(path = "/api")
+public class UserController {
+
+}
