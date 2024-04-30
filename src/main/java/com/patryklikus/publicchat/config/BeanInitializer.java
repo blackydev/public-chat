b5118731f50726992b7e39490123c351268582f8
Blackky <patryklikus@icloud.com>
2024-04-30 22:10:42 +0200

    improvements

--- src/main/java/com/patryklikus/publicchat/config/BeanInitializer.java
+++ src/main/java/com/patryklikus/publicchat/config/BeanInitializer.java
@@ -1,11 +1,12 @@
 /* Copyright Patryk Likus All Rights Reserved. */
 package com.patryklikus.publicchat.config;
 
-import static com.patryklikus.publicchat.config.BeanProvider.*;
-
 import com.patryklikus.publicchat.https.RequestHandlersManager;
 import com.sun.net.httpserver.HttpServer;
+
 import java.sql.SQLException;
+
+import static com.patryklikus.publicchat.config.BeanProvider.*;
 
 public class BeanInitializer {
     public static void initBeans(HttpServer server) throws SQLException {
@@ -22,11 +23,7 @@ public class BeanInitializer {
     private static void initEndpoints(HttpServer server) {
         var requestHandlersManager = new RequestHandlersManager(server, AUTH_SERVICE);
         requestHandlersManager.addControllers(
-                PUBLIC_CONTROLLER,
-                PAGE_CONTROLLER,
-                USER_CONTROLLER,
-                MESSAGE_CONTROLLER,
-                AUTH_CONTROLLER
+                PUBLIC_CONTROLLER, PAGE_CONTROLLER, USER_CONTROLLER, MESSAGE_CONTROLLER, AUTH_CONTROLLER
         );
         requestHandlersManager.init();
     }
